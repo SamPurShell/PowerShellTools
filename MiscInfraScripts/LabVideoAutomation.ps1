f5ab434a0b50525fbbe67f8eb105415a006b0b8d
SamPurShell <sam@thepursissons.com>
2025-02-24 15:48:51 -0800

    Update LabVideoAutomation.ps1

--- MiscInfraScripts/LabVideoAutomation.ps1
+++ MiscInfraScripts/LabVideoAutomation.ps1
@@ -13,154 +13,154 @@
 $RunningConfig = import-csv -path "\\PathToConfigFile\Config.csv"
 ForEach ($run in $RunningConfig)
 {
-$LocalPath = $run.LocalPath
+  $LocalPath = $run.LocalPath
 
-$RemotePath = $rfun.RemotePath
+  $RemotePath = $rfun.RemotePath
 
-$AdditionalRemotePath = $run.AdditionalRemotePath
+  $AdditionalRemotePath = $run.AdditionalRemotePath
 
-$Destination = $run.Destination
+  $Destination = $run.Destination
 
-$FTPHostName = $run.FTPHostName
+  $FTPHostName = $run.FTPHostName
 
-$FTPMode = $run.FTPMode
+  $FTPMode = $run.FTPMode
 
-$FTPMode = [System.Convert]::ToBoolean($run.FTPMode)
+  $FTPMode = [System.Convert]::ToBoolean($run.FTPMode)
 
-#Generate the time stamps for logging purposes
-$LogDate = (Get-Date -Format 'MM-dd-yyyy').ToString() + ".log"
-$timeStamp = Get-Date -Format 'hh:mm:ss-MM-dd-yyyy'
+  #Generate the time stamps for logging purposes
+  $LogDate = (Get-Date -Format 'MM-dd-yyyy').ToString() + ".log"
+  $timeStamp = Get-Date -Format 'hh:mm:ss-MM-dd-yyyy'
 
-#Begin logging to the folder
-Start-Transcript ("C:\Logs\Lab Video Automation\LabVideoAutomation - " + $LogDate) -append -Force
-Write-Output "$($timeStamp) Run Started!"
+  #Begin logging to the folder
+  Start-Transcript ("C:\Logs\Lab Video Automation\LabVideoAutomation - " + $LogDate) -append -Force
+  Write-Output "$($timeStamp) Run Started!"
 
-#Test if required folders exist on server and client
-$ServerFolderExistsTest = Test-Path $LocalPath
-$ClientFolderExistsTest = Test-path $RemotePath
-$DestinationFolderExistsTest = Test-path $Destination
+  #Test if required folders exist on server and client
+  $ServerFolderExistsTest = Test-Path $LocalPath
+  $ClientFolderExistsTest = Test-path $RemotePath
+  $DestinationFolderExistsTest = Test-path $Destination
 
 
-If ($ServerFolderExistsTest -eq $False)
-{
-New-Item $LocalPath -ItemType Directory
-}
+  If ($ServerFolderExistsTest -eq $False)
+  {
+  New-Item $LocalPath -ItemType Directory
+  }
 
-If ($ClientFolderExistsTest -eq $False)
-{
-New-Item $Remotepath -ItemType Directory
-}
+  If ($ClientFolderExistsTest -eq $False)
+  {
+  New-Item $Remotepath -ItemType Directory
+  }
 
-If ($DestinationFolderExistsTest -eq $False)
-{
-New-Item $Destination -ItemType Directory
-}
+  If ($DestinationFolderExistsTest -eq $False)
+  {
+  New-Item $Destination -ItemType Directory
+  }
 
-#Test if remote path is reachable, used to send an email in separate function
-$PathExistsTest = Test-path $RemotePath
+  #Test if remote path is reachable, used to send an email in separate function
+  $PathExistsTest = Test-path $RemotePath
 
-##Load "CG Email" function if test fails
-If ($PathExistsTest -eq $False)
-{  
+  ##Load "CG Email" function if test fails
+  If ($PathExistsTest -eq $False)
+  {  
 
-#Convert $localpath to array with splitting to get device name for email
-$LocalPathArray = $LocalPath.split("/")
-$DeviceNetBIOS = $LocalPathArray[2]
+  #Convert $localpath to array with splitting to get device name for email
+  $LocalPathArray = $LocalPath.split("/")
+  $DeviceNetBIOS = $LocalPathArray[2]
 
-#Create the email message body HTML
-$message="<b> <font color=red> <h1 style=font-size:200%> ALERT!!! </font> </h1> </mark></b> Device $($DeviceNetBIOS) was unreachable at $($timestamp). The nightly video compression job failed on this device as a result. Please check that the computer/device is online. </mark></b><br /><br /> Thank you! </mark></b><br /><br /><b>Technology Services </b><br />CorneaGen <br />"
+  #Create the email message body HTML
+  $message="<b> <font color=red> <h1 style=font-size:200%> ALERT!!! </font> </h1> </mark></b> Device $($DeviceNetBIOS) was unreachable at $($timestamp). The nightly video compression job failed on this device as a result. Please check that the computer/device is online. </mark></b><br /><br /> Thank you! </mark></b><br /><br /><b>Technology Services </b><br />CorneaGen <br />"
 
-#Send the email
-#Optional - you can put details of sending an email from domain to a recipient here if you would like
-}
+  #Send the email
+  #Optional - you can put details of sending an email from domain to a recipient here if you would like
+  }
 
-# Import installed module WinSCP (uses WMF5)
-If ($FTPMode -eq $True)
-{
-Import-Module WinSCP
-}
+  # Import installed module WinSCP (uses WMF5)
+  If ($FTPMode -eq $True)
+  {
+  Import-Module WinSCP
+  }
 
-# Define variables for FTP session
-If ($FTPMode -eq $True)
-{
-$username = "UsernameToRecordersFTPServer"                                                                                             
-$password = ConvertTo-SecureString "PasswordToRecordersFTPServer" -AsPlainText -Force                                                   
-$credential = New-Object -typename System.Management.Automation.PSCredential -argumentlist $username, $password 
-$sessionOption = New-WinSCPSessionOption -HostName $FTPHostName -Protocol sFTP -Credential $credential
-}
+  # Define variables for FTP session
+  If ($FTPMode -eq $True)
+  {
+  $username = "UsernameToRecordersFTPServer"                                                                                             
+  $password = ConvertTo-SecureString "PasswordToRecordersFTPServer" -AsPlainText -Force                                                   
+  $credential = New-Object -typename System.Management.Automation.PSCredential -argumentlist $username, $password 
+  $sessionOption = New-WinSCPSessionOption -HostName $FTPHostName -Protocol sFTP -Credential $credential
+  }
 
-# Start the session
-If ($FTPMode -eq $True)
-{
-New-WinSCPSession -SessionOption $sessionOption
-}
-#Move .mp4 files on the recorder to a local directory
-If ($FTPMode -eq $True)
-{
-$FormattedFilter = ("*." + $Filter)
-$RemotePathWithFilter = $($RemotePath) + "/" + $($FormattedFilter)
-If ($AdditionalRemotePath -ne '')
-{
-$RemotePathWithFilter = (($RemotePath) + "/" + ($FormattedFilter)), (($AdditionalRemotePath) + "/"  + ($FormattedFilter))
-}
-Receive-WinSCPItem -RemotePath $RemotePathWithFilter -LocalPath $LocalPath
-}
-If ($FTPMode -eq $False)
-{
-Copy-Item -Path $(($RemotePath) + "/*")  -Destination $LocalPath
-}
-#Set up Variables, SHOULD UPDATE THIS WITH A SWITCH
-$LocalFiles = get-childitem  -Path $LocalPath -Include *.* -Recurse 
-If (($FTPMode -eq $True) -and ($AdditionalRemotePath -ne ''))
-{
-$RemoteFiles = Get-WinSCPChildItem -path $RemotePath, $AdditionalRemotePath -filter $FormattedFilter
-}
-If (($FTPMode -eq $True) -and ($AdditionalRemotePath -eq ''))
-{
-$RemoteFiles = Get-WinSCPChildItem -path $RemotePath -Filter $FormattedFilter
-}  
-If (($FTPMode -eq $False) -and ($AdditionalRemotePath -ne ''))
-{
-$RemoteFiles = Get-ChildItem -path $RemotePath, $AdditionalRemotePath
-}    
-If (($FTPMode -eq $False) -and ($AdditionalRemotePath -eq ''))   
-{
-$RemoteFiles = Get-ChildItem -path $RemotePath
-}
- 
-#Check Local Files and Remote Files and delete files that exist in both places from the recorder.
-
-ForEach ($LocalFile in $LocalFiles)
-{
-    ForEach ($RemoteFile in $RemoteFiles)
+  # Start the session
+  If ($FTPMode -eq $True)
+  {
+  New-WinSCPSession -SessionOption $sessionOption
+  }
+  #Move .mp4 files on the recorder to a local directory
+  If ($FTPMode -eq $True)
+  {
+  $FormattedFilter = ("*." + $Filter)
+  $RemotePathWithFilter = $($RemotePath) + "/" + $($FormattedFilter)
+    If ($AdditionalRemotePath -ne '')
     {
-        if (($LocalFile.name -match $RemoteFile.name) -and ($LocalFile.length -match $RemoteFile.Length))
-        {
-            Write-Output "$($timeStamp) $($RemoteFile.FullName) was transferred to server, deleting it from the remote directory."
-            If ($FTPMode -eq $True)
-            {
-            Remove-WinSCPItem $RemoteFile.FullName -Confirm:$False
-            }
-            If ($FTPMode -eq $False)
-            {
-            Remove-Item $RemoteFile.FullName -Confirm:$False
-            }
-            }
-            }
-            }
-            
-#Use Handbrake CLI to save new compressed file and delete the old file
-Write-Output "$($timeStamp) Starting video compression jobs..."
-$filelist = $localfiles 
+  $RemotePathWithFilter = (($RemotePath) + "/" + ($FormattedFilter)), (($AdditionalRemotePath) + "/"  + ($FormattedFilter))
+    }
+  Receive-WinSCPItem -RemotePath $RemotePathWithFilter -LocalPath $LocalPath
+  }
+  If ($FTPMode -eq $False)
+  {
+  Copy-Item -Path $(($RemotePath) + "/*")  -Destination $LocalPath
+  }
+  #Set up Variables, SHOULD UPDATE THIS WITH A SWITCH
+  $LocalFiles = get-childitem  -Path $LocalPath -Include *.* -Recurse 
+  If (($FTPMode -eq $True) -and ($AdditionalRemotePath -ne ''))
+  {
+$RemoteFiles = Get-WinSCPChildItem -path $RemotePath, $AdditionalRemotePath -filter $FormattedFilter
+  }
+  If (($FTPMode -eq $True) -and ($AdditionalRemotePath -eq ''))
+  {
+  $RemoteFiles = Get-WinSCPChildItem -path $RemotePath -Filter $FormattedFilter
+  }  
+  If (($FTPMode -eq $False) -and ($AdditionalRemotePath -ne ''))
+  {
+  $RemoteFiles = Get-ChildItem -path $RemotePath, $AdditionalRemotePath
+  }    
+  If (($FTPMode -eq $False) -and ($AdditionalRemotePath -eq ''))   
+  {
+  $RemoteFiles = Get-ChildItem -path $RemotePath
+  }
+ 
+  #Check Local Files and Remote Files and delete files that exist in both places from the recorder.
 
-$date = (Get-Date -Format 'MM-dd-yyyy')
+  ForEach ($LocalFile in $LocalFiles)
+  {
+      ForEach ($RemoteFile in $RemoteFiles)
+      {
+          if (($LocalFile.name -match $RemoteFile.name) -and ($LocalFile.length -match $RemoteFile.Length))
+          {
+              Write-Output "$($timeStamp) $($RemoteFile.FullName) was transferred to server, deleting it from the remote directory."
+              If ($FTPMode -eq $True)
+              {
+              Remove-WinSCPItem $RemoteFile.FullName -Confirm:$False
+              }
+              If ($FTPMode -eq $False)
+              {
+              Remove-Item $RemoteFile.FullName -Confirm:$False
+              }
+          }
+      }
+    }
+            
+  #Use Handbrake CLI to save new compressed file and delete the old file
+  Write-Output "$($timeStamp) Starting video compression jobs..."
+  $filelist = $localfiles 
+
+  $date = (Get-Date -Format 'MM-dd-yyyy')
  
-$num = $filelist | measure
-$filecount = $num.count
+  $num = $filelist | measure
+  $filecount = $num.count
  
-$i = 0;
-ForEach ($file in $filelist)
-{
+  $i = 0;
+  ForEach ($file in $filelist)
+  {
     $i++;
     $oldfile = $file.DirectoryName + "\" + $file.BaseName + $file.Extension;
     $newfile = $file.DirectoryName + "\" + $file.BaseName + " Compressed Video - " + $date + $file.Extension;
@@ -174,18 +174,18 @@ ForEach ($file in $filelist)
     Start-Process "C:\HandBrake CLI\HandBrakeCLI.exe" -ArgumentList "-i `"$oldfile`" -t 1 --angle 1 -c 1 -o `"$newfile`" -f mp4  -O  --decomb --modulus 16 -e x264 -q 20 --vfr -a 1 -6 dpl2 -R Auto -D 0 --gain 0 --verbose=0" -Wait -NoNewWindow
     Remove-Item $oldfile -confirm:$False
     Write-Output "$($timeStamp) Created new compressed file $($newfile), removed $($oldfile)"
-}
-Write-Output "$($timeStamp) Video compression finshed!"
+  }
+  Write-Output "$($timeStamp) Video compression finshed!"
 
-#Move compressed files to destination on File Server
-$LocalFiles = get-childitem  -Path $LocalPath -Recurse
-ForEach ($LocalFile in $LocalFiles)
+  #Move compressed files to destination on File Server
+  $LocalFiles = get-childitem  -Path $LocalPath -Recurse
+  ForEach ($LocalFile in $LocalFiles)
     {
     $LocalFile | Move-Item -Destination $Destination
     Write-Output "$($timeStamp) Moved $($LocalFile.FullName) to $($Destination)"
     }
 
-#Stop Logging
-Write-Output "$($timeStamp) Run Completed!"
-Stop-Transcript
+  #Stop Logging
+  Write-Output "$($timeStamp) Run Completed!"
+  Stop-Transcript
 }
